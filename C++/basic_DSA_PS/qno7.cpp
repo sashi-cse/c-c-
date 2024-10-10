9eb2d02eb6043a3e5f822303e1223682eaa6afbb
sashi-cse <sashikant112005@gmail.com>
2024-10-10 22:40:08 +0530

    basicDSA

---
+++ C++/basic_DSA_PS/qno7.cpp
@@ -0,0 +1,48 @@
+/*7. **Crafting Star Patterns**  
+   **Difficulty**: Easy  
+   **Topics**: Basic Programming, Patterns  
+   **Description**: Write a program to create different star patterns (e.g., pyramid, diamond).  
+   **Example**:  
+   Input: `patternType = "pyramid", height = 5`  
+   Output:  
+   ```
+       *
+      ***
+     *****
+    *******
+   *********
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    cin>>n;
+
+    int i = 1;
+    while(i<=n){
+
+        int space = n-i;
+        while(space){
+            cout<<" ";
+            space--;
+        }
+
+        int j = 1;
+        while(j<=i){
+            cout<<"*";
+            j++;
+        }
+        // for(int j=2;j<=i;j++){
+        //     cout<<"*";
+        // }
+        int k=2;
+        while(k<=i){
+            cout<<"*";
+            k++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
