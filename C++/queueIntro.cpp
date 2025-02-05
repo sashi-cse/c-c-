df98c9c3019467afab04f8593a3c06ba82e31025
sashi-cse <sashikant112005@gmail.com>
2025-02-05 20:25:37 +0530

    queue

--- C++/queueIntro.cpp
+++ C++/queueIntro.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main(){
 
+    deque<int> d;
+
+    d.push_front(12);
+    d.push_back(14);
+
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+
+    d.pop_front();
+
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+
+    d.pop_back();
+
+    if(d.empty()) {
+        cout << "queue is empty" << endl;
+    }
+    else{
+        cout << "queue is not empty" << endl;
+    }
+
+    /*
     //create a queue
     queue<int> q;
 
@@ -26,7 +49,7 @@ int main(){
     }
     else{
         cout << "Queue is not empty " << endl;
-    }
+    }*/
 
     return 0;
 }
